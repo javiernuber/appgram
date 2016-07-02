d39dd3faf0fb9802bdc5eee2dfa025ee03a45a01
Javier Núñez Berrocoso <javiernuber@gmail.com>
2016-07-02 02:59:35 +0200

    Ventana de signin

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,31 +1,28 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
 
-var signupForm = yo`<div class="col s12 m7">
+var signinForm = yo`<div class="col s12 m7">
 							<div class="row">
 								<div class="signup-box">
 									<h1 class="title">JSGRAM</h1>
 									<form class="signup-form">
-										<h2>Registrate</h2>
 										<div class="section">
 											<a class="btn btn-fb hide-on-small-only">Iniciar sesión en Facebook</a>
 											<a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
 										</div>
 										<div class="divider"></div>
 										<div class="section"></div>
-											<input type="email" name="email" placeholder="Correo electrónico"/>
-											<input type="text" name="name" placeholder="Nombre completo"/>
 											<input type="text" name="username" placeholder="Nombre de usuario"/>
 											<input type="password" name="pasword" placeholder="Contraseña"/>
-											<button class="btn waves-effect waves-light">Regístrate</button>
+											<button class="btn waves-effect waves-light">Inicia sesión</button>
 									</form>	
 								</div>
 							</div>
 							<div class="row">
 								<div class="login-box">
-									¿Tienes una cuenta? <a href="/signin">Entrar</a>
+									¿No tienes una cuenta? <a href="/signup">Registrate</a>
 								</div>
 							</div>`
 
-module.exports = landing(signupForm);
+module.exports = landing(signinForm);
 
